3b8aafd15d4366941a1757f0dcec859f194102bb
cocohub <gussahom@student.gu.se>
2018-04-19 02:00:32 +0200

    moved working templates to new dir

--- examples/csharpExample/Handler.cs
+++ examples/csharpExample/Handler.cs
@@ -1,39 +1,17 @@
+using System; 
 using Amazon.Lambda.Core;
-using System;
+using Amazon.Lambda.Serialization.Json;
+using Amazon.Lambda.APIGatewayEvents;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace AwsDotnetCsharp
-{
-    public class Handler
-    {
-       public Response Hello(Request request)
-       {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
-       }
-    }
-
-    public class Response
-    {
-      public string Message {get; set;}
-      public Request Request {get; set;}
-
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
-    }
-
-    public class Request
-    {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
-
-      public Request(string key1, string key2, string key3){
-        Key1 = key1;
-        Key2 = key2;
-        Key3 = key3;
-      }
-    }
+namespace AwsDotnetCsharp { 
+    public class Handler { 
+        public APIGatewayProxyResponse Hello(APIGatewayProxyRequest request) { 
+            return new APIGatewayProxyResponse() {
+                StatusCode = 200, 
+                Body = "Go Serverless v1.0! Your function executed successfully!", 
+            }; 
+        }    
+    } 
 }
