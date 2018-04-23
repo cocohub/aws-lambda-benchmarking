e77ed4eb66bb1c1abe689fbf876ca78101cc9e29
cocohub <gussahom@student.gu.se>
2018-04-24 00:10:50 +0200

    changed location

--- tools/test.py
+++ tools/test.py
@@ -1,20 +1,21 @@
-import sys, os
+import sys, os, datetime
 
 #creates the system command to execute artillery benchmarks
-def createCommand(xExecutions, xPeople, url):
-    return "artillery quick --count %s -n %s %s" % (xExecutions, xPeople, url)
+def createCommand(xExecutions, xPeople, extras, url):
+    return "artillery quick --count %s -n %s %s %s" % (xExecutions, xPeople, extras, url)
 
 #file containing the language and url separated by "|"
-fileLinks = "sortlinks.txt"
+fileLinks = "links.txt"
 
 #amount of executions and people executing
 xExecutions = sys.argv[1]
 xPeople = sys.argv[2]
+maxExecutions = 1000
 
 #if we have too many executions, cancel script to save executions
-if int(xExecutions) * int(xPeople) >= 50:
+if int(xExecutions) * int(xPeople) >= maxExecutions:
     print("*** ERROR ***")
-    print("You're doing too many requests, keep it lower than 50")
+    print("You're doing too many requests, keep it lower than %d" % (maxExecutions))
     sys.exit(0)
 else:
     #otherwise, go ahead
@@ -26,6 +27,9 @@ with open(fileLinks) as f:
     for line in f:
         language = line.split("|")[0]
         url = line.split("|")[1]
+        time = datetime.datetime.now().strftime("%Y-%m-%d-%I:%M:%S")
+        #output = "-o %s%s" % (language,time + ".json")
+        output = "-o %s" % (language + ".json")
         
         print("*** TESTING %s ***" % (language.upper()))
-        os.system(createCommand(xExecutions,xPeople,url))
+        os.system(createCommand(xExecutions,xPeople, output, url))
