3bba636971f8472384b47d9ea6c730a80e71ad62
cocohub <gussahom@student.gu.se>
2018-04-24 00:10:17 +0200

    working tool

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,23 +1,67 @@
 import sys, os
 
-#creates the system command to deploy
-def createCommand(xExecutions, xPeople, url):
-    return "sls deploy"
-
-#amount of executions and people executing
-option = sys.argv[1]
-todoString = ""
-if option.strip() == "-a":
-    todoString = ", DEPLOYING EVERYTHING"
-
-print("*** STARTING DEPLOYMENT%s ***\n" % (todoString))
-
 deployFolder = os.getcwd() + "\\main\\"
 #print(deployFolder)
 
-#todo
-#if java in dir, mvn package, then sls deploy
-for dir in os.listdir(deployFolder):
-    os.chdir(deployFolder + dir)
-    os.system("sls deploy")
+javaDir = "javaSort"
+nodejsDir = "nodejsSort"
+pythonDir = "pythonSort"
+csharpDir = "csharpSort"
+
+#creates the system command to deploy
+def cmd(command):
+    os.system(command)
+
+def sls_deploy():
+    cmd("sls deploy")
+    
+def deployJava():
+    cmd("mvn package")
+    sls_deploy()
+    
+def deployCsharp():
+    cmd("build.cmd")
+    sls_deploy()
+    
+def deployAll():
+    for dir in os.listdir(deployFolder):
+        os.chdir(deployFolder + dir)
+        if "java" in dir:
+            deployJava()
+        elif "csharp" in dir:
+            deployCsharp()
+        else:
+            sls_deploy()
+
+option = ""
+
+try:
+    option = sys.argv[1]
+except IndexError:
+    option = "" #if we have no option, we'll deploy everything
+
+if option.strip() == "-a" or option.strip() == "":
+    todoString = ", DEPLOYING EVERYTHING"
+    print("*** STARTING DEPLOYMENT%s ***\n" % (todoString))
+    deployAll()
+elif option.strip() == "-java":
+    todoString = ", DEPLOYING JAVA"
+    print("*** STARTING DEPLOYMENT%s ***\n" % (todoString))
+    os.chdir(deployFolder + javaDir)
+    deployJava()
+elif option.strip() == "-nodejs":
+    todoString = ", DEPLOYING NODEJS"
+    print("*** STARTING DEPLOYMENT%s ***\n" % (todoString))
+    os.chdir(deployFolder + nodejsDir)
+    sls_deploy()
+elif option.strip() == "-python":
+    todoString = ", DEPLOYING PYTHON"
+    print("*** STARTING DEPLOYMENT%s ***\n" % (todoString))
+    os.chdir(deployFolder + pythonDir)
+    sls_deploy()
+elif option.strip() == "-csharp":
+    todoString = ", DEPLOYING C#"
+    print("*** STARTING DEPLOYMENT%s ***\n" % (todoString))
+    os.chdir(deployFolder + csharpDir)
+    deployCsharp()
 
